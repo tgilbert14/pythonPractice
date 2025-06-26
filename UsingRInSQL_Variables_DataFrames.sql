6a5b3b6d397a85fc69bc67f4fe194c279d967b34
Timothy Gilbert <tsgilbert35@hotmail.com>
2025-06-26 13:36:42 -0700

    finished data selection section

--- UsingRInSQL_Variables_DataFrames.sql
+++ UsingRInSQL_Variables_DataFrames.sql
@@ -69,6 +69,7 @@ FROM Application.Countries;'
 
 
 -- getting data back to sql server to use
+-- must call it 'OutputDataSet'
 EXECUTE sp_execute_external_script
 @language = N'R',
 @script = N'
@@ -84,23 +85,65 @@ FROM Application.Countries;'
 WITH RESULT SETS (([Country ID] int, [Country Name] nvarchar(60), [Continent] nvarchar(30), [Region] nvarchar(30),
 [IsoNumericCode] int))
 
-
-
-
-
-
-
--- Convert Celsius to Fahrenheit
-EXEC sp_execute_external_script
+-- full data example
+EXECUTE sp_execute_external_script
 @language = N'R',
 @script = N'
-Celsius <- InputDataSet[,1]
-Fahrenheit <- (Celsius * 1.8) + 32
-OutputDataSet <- data.frame(Celsius, Fahrenheit)
+OutputDataSet <- InputDataSet
 ',
-@input_data_1 = N'SELECT Temperature FROM Warehouse.ColdRoomTemperatures;'
-WITH RESULT SETS (([Degrees C] decimal(10,2), [Degrees F] decimal(10,2)));
+@input_data_1 = N'SELECT CountryID, CountryName, Continent, Region, IsoNumericCode
+FROM Application.Countries;'
+-- need this part to get col names - in order of query, assign datatypes that sql supports
+WITH RESULT SETS (([Country ID] int, [Country Name] nvarchar(60), [Continent] nvarchar(30), [Region] nvarchar(30),
+[IsoNumericCode] int))
+
+-- selecting data
+EXECUTE sp_execute_external_script
+@language = N'R',
+@script = N'
+#OutputDataSet <- data.frame(InputDataSet$CountryName)
+OutputDataSet <- data.frame(unique(InputDataSet[,c("CountryName","Continent")]))
+',
+@input_data_1 = N'SELECT CountryID, CountryName, Continent, Region, IsoNumericCode
+FROM Application.Countries;'
+-- need this part to get col names - in order of query, assign datatypes that sql supports
+WITH RESULT SETS ((CountryName nvarchar(60), Continent nvarchar(30)))
 
 
--- Another approach does the same thing on a single line. Try:
--- OutputDataSet <- data.frame(InputDataSet[,1], (InputDataSet[,1] * 1.8) + 32)
+-- combining vectors -->
+EXECUTE sp_execute_external_script
+@language = N'R',
+@script = N'
+ItemName <- InputDataSet[,1]
+OldPrice <- InputDataSet[,2]
+# example of price with 50% discount
+NewPrice <- OldPrice * .5
+OutputDataSet <- data.frame(ItemName, OldPrice, NewPrice)
+',
+@input_data_1 = N'SELECT StockItemName, RecommendedRetailPrice
+FROM Warehouse.StockItems
+WHERE SupplierID = 1;'
+WITH RESULT SETS (([Item Name] nvarchar(100), [Old Price] decimal(8,2), [New Price] decimal(8,2)));
+
+-- subset filter
+EXECUTE sp_execute_external_script
+@language = N'R',
+@script = N'
+OutputDataSet <- subset(InputDataSet, SupplierID == 1, )
+',
+@input_data_1 = N' SELECT StockItemName, SupplierID FROM Warehouse.StockItems; '
+WITH RESULT SETS (([StockItemName] nvarchar(100), [SupplierID] int))
+
+-- example test - convert C to F temp...
+-- formula1, F = (C * 1.8) + 32
+-- formula2, C = (F - 32) / 1.8
+EXECUTE sp_execute_external_script
+@language = N'R',
+@script = N'
+Temp_C <- InputDataSet[,1]
+Converted <- (Temp_C * 1.8) + 32
+Temp_F <- Converted
+OutputDataSet <- data.frame(Temp_C, Temp_F)
+',
+@input_data_1 = N'SELECT Temperature FROM Warehouse.ColdRoomTemperatures'
+WITH RESULT SETS (([Temp Celsius] float, [Temp Fahrenheit] decimal(10,2)))
